5b26aedb9f19486d233eb6ec68e956c67b581d40
frannlima <franlimabreu87@gmail.com>
2026-09-25 02:05:47 -0300

    Renova cache e força navegação atual no GitHub Pages

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-const CACHE='estore-cepi-shell-v28';
+const CACHE='estore-cepi-shell-v29';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(SHELL.map(u=>new Request(u,{cache:'reload'})))).catch(()=>{})
+  );
 });
 
 self.addEventListener('activate',event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
 
@@ -17,10 +21,31 @@ self.addEventListener('fetch',event=>{
   if(req.method!=='GET') return;
   const url=new URL(req.url);
   if(url.origin!==self.location.origin) return;
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(new Request(req,{cache:'no-store'}))
+        .then(res=>{
+          if(res&&res.ok){
+            const copy=res.clone();
+            caches.open(CACHE).then(cache=>cache.put('./index.html',copy)).catch(()=>{});
+          }
+          return res;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    fetch(req).then(res=>{
-      if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{})}
-      return res;
-    }).catch(()=>caches.match(req).then(hit=>hit||caches.match('./index.html')))
+    fetch(req)
+      .then(res=>{
+        if(res&&res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
+        }
+        return res;
+      })
+      .catch(()=>caches.match(req))
   );
 });
